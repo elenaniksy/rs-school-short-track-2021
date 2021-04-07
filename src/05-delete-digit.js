d7d26a1b76894ee274cca5ba4ed59cc0afc27dd4
elenaniksy <elfhelen@gmail.com>
2021-04-07 19:46:19 +0300

    feat: add solution 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,14 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const nToStr = n.toString();
+  const numberCombinations = [];
+  for (let i = 0; i < nToStr.length; i++) {
+    const num = nToStr.slice(0, i) + nToStr.slice(i + 1);
+    numberCombinations.push(parseInt(num, 10));
+  }
+  return Math.max.apply(null, numberCombinations);
 }
 
 module.exports = deleteDigit;
