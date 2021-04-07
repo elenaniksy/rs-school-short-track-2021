f8895ba2d8eabaf0f26b4d5a54f642a9dd879f18
elenaniksy <elfhelen@gmail.com>
2021-04-07 17:36:30 +0300

    feat: add solution to 04-mine-swepeer

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,45 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let y = 0; y < matrix.length; y++) {
+    const row = [];
+    for (let x = 0; x < matrix[y].length; x++) {
+      let minecount = 0;
+      if (y !== 0 && matrix[y - 1][x] === true) { // строго над
+        minecount++;
+      }
+      if (y !== (matrix.length - 1) && matrix[y + 1][x] === true) { // строго под
+        minecount++;
+      }
+      if (x !== 0 && matrix[y][x - 1] === true) { // слева
+        minecount++;
+      }
+      if (matrix[y][x + 1] === true) { // справа
+        minecount++;
+      }
+      if (y !== 0 && x !== 0 && matrix[y - 1][x - 1] === true) { // по диагонали вверх влево
+        minecount++;
+      }
+      if (y !== (matrix.length - 1) && x
+        !== (matrix[0].length - 1) && matrix[y + 1][x + 1] === true) { // по диагонали вниз
+        // вправо
+        minecount++;
+      }
+      if (y !== 0 && matrix[y - 1][x + 1] === true) { // по диагонали вверх вправо
+        minecount++;
+      }
+      if (y !== (matrix.length - 1) && x !== 0 && matrix[y + 1][x - 1] === true) { // по диагонали
+        // вниз влево
+        minecount++;
+      }
+      row.push(minecount);
+    }
+    result.push(row);
+  }
+  return result;
 }
 
 module.exports = minesweeper;
